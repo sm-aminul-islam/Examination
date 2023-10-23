baf031638e98bb454ebeb4f0e5234e907d03c43e
Aminul <robinraja42@gmail.com>
2023-10-23 10:58:46 +0600

    State provider

--- Riverpod_Demo_Project/riverpod_demo_project/lib/main.dart
+++ Riverpod_Demo_Project/riverpod_demo_project/lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
+
 import 'package:riverpod_demo_project/modules/home/view/homepage.dart';
+
+final countProvider = StateProvider<int>((ref) => 0);
 
 void main() {
   runApp(
-    ProviderScope(
+    const ProviderScope(
       child: MyApp(),
     ),
   );
--- Riverpod_Demo_Project/riverpod_demo_project/lib/modules/home/view/homepage.dart
+++ Riverpod_Demo_Project/riverpod_demo_project/lib/modules/home/view/homepage.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_demo_project/main.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends ConsumerWidget {
   const Homepage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final value = ref.watch(countProvider);
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "Riverpod Demo",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Center(
+        child: Text("Provider say $value"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.read(countProvider.state).state++;
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
