e33355c50f296d194e1bf95928568c1ecc0c9f21
Aminul <robinraja42@gmail.com>
2023-10-23 10:08:25 +0600

    Learning Riverpod state management

---
+++ Riverpod_Demo_Project/riverpod_demo_project/android/app/src/main/kotlin/com/example/riverpod_demo_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.riverpod_demo_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Riverpod_Demo_Project/riverpod_demo_project/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:riverpod_demo_project/modules/home/view/homepage.dart';
+
+void main() {
+  runApp(
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Riverpod Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        debugShowCheckedModeBanner: false,
+        home: Homepage());
+  }
+}
